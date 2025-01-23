fabb1d44375b8b4e8cf6c4db0ca1cc56aaf4afdd
gabriel fonseca <gabrielsillva356@gmail.com>
2025-01-22 23:30:20 -0300

    troquei o formulario para o componente home

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component,OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+
 
 
 
@@ -8,40 +8,6 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'Angular';
-  formulario!: FormGroup;
-  data: any[] = []
-
-  ngOnInit(): void {
-    this.formulario = new FormGroup({
-      name: new FormControl("",[Validators.required, Validators.minLength(5)]),
-      lastname: new FormControl("",[Validators.required, Validators.maxLength(5)]),
-      email: new FormControl("",[Validators.required, Validators.email]),
-    })
-  }
-
-  get name(){
-    return this.formulario.get('name')!;
-  }
-  get lastname(){
-    return this.formulario.get('lastname')!;
-  }
-  get email(){
-    return this.formulario.get('email')!;
-  }
-
-  submit(){
-    
-      this.data.push(this.formulario.value)
-      console.log(this.data)
-
-      this.formulario = new FormGroup({
-        name: new FormControl(""),
-        lastname: new FormControl(""),
-        email: new FormControl(""),
-      })
-    
-  }
+export class AppComponent { 
 
 }
--- src/app/componentes/home/home.component.ts
+++ src/app/componentes/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-home',
@@ -6,5 +8,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
-
+   formulario!: FormGroup;
+    data: any[] = []
+  
+    ngOnInit(): void {
+      this.formulario = new FormGroup({
+        name: new FormControl("",[Validators.required, Validators.minLength(5)]),
+        lastname: new FormControl("",[Validators.required, Validators.maxLength(5)]),
+        email: new FormControl("",[Validators.required, Validators.email]),
+      })
+    }
+  
+    get name(){
+      return this.formulario.get('name')!;
+    }
+    get lastname(){
+      return this.formulario.get('lastname')!;
+    }
+    get email(){
+      return this.formulario.get('email')!;
+    }
+  
+    submit(){
+      
+        this.data.push(this.formulario.value)
+        console.log(this.data)
+  
+        this.formulario = new FormGroup({
+          name: new FormControl(""),
+          lastname: new FormControl(""),
+          email: new FormControl(""),
+        })
+      
+    }
 }
