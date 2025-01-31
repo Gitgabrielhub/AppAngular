2781e665b54f35c9b9bf9ce4b27f23fdacf47006
gabriel fonseca <gabrielsillva356@gmail.com>
2025-01-31 01:41:39 -0300

    filtro

--- src/app/componentes/negocicoes/negocicoes.component.ts
+++ src/app/componentes/negocicoes/negocicoes.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { map } from 'rxjs';
 import { FinanceApiService } from 'src/app/services/finance-api.service';
+
 
 
 @Component({
@@ -10,12 +12,38 @@ import { FinanceApiService } from 'src/app/services/finance-api.service';
 })
 export class NegocicoesComponent {
   prices: any[] =[]; 
+  moedas: any[] =[]; 
+  formulario!:FormGroup;
+
   constructor(private financeApi: FinanceApiService) { }
   ngOnInit(): void {
     this.financeApi.getNegociacoes().subscribe((data) => {
       this.prices = data
+      this.moedas = data.map((item:any) => item.symbol)
       this.prices.map((item) => {
-        console.log(item.price = parseFloat(item.price).toFixed(2))
+        console.log(item.price = parseFloat(item.price).toFixed(2), item.symbol)
+      })
+    })
+
+    this.formulario = new FormGroup(
+      {
+        moedas: new FormControl('',[Validators.required])
+      }
+    )
+      
+  }
+
+  getMoedas(){
+    return this.formulario.get('moedas');
+  }
+  pesquisar(){
+    
+    this.financeApi.getNegociacoes().subscribe((data)=>{
+      this.moedas = data.map((item:any) => item.symbol.toLowerCase(), );
+      this.moedas.filter((item:any) =>{
+        if(item === this.formulario.value){
+          console.log(item.symbol)
+        }
       })
     })
   }
