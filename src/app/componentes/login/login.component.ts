0f943381a207a8a60fdad038d49709cade1c9f2a
gabriel fonseca <gabrielsillva356@gmail.com>
2025-02-18 13:32:42 -0300

    implementação de validators

--- src/app/componentes/login/login.component.ts
+++ src/app/componentes/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+  formulario!:FormGroup;
+  dataForm:any[]=[]; 
 
+  ngOnInit(): void {
+    this.formulario = new FormGroup({
+      email: new FormControl("",[Validators.required, Validators.email]),
+      password: new FormControl("",[Validators.required, Validators.minLength(8)])
+    })
+  }
+
+  getEmail(){
+    return this.formulario.get('email')
+  }
+  private getPassword(){
+    return this.formulario.get('password')
+  }
+
+  enviarForm(e:Event){
+    this.dataForm.push(this.formulario.value)
+    console.log(this.dataForm)
+  }
 }
